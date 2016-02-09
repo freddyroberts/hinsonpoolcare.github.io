d887499899bc7a635dedd0cbf98491c223ff8836
Freddy Roberts <freddyroberts9@gmail.com>
2016-02-08 23:33:08 -0600

    added form to estimate page

--- src/js/estimates.js
+++ src/js/estimates.js
@@ -3,19 +3,21 @@ import React, { PropTypes } from 'react'
 class Estimates extends React.Component {
   render () {
     return (
-      <main>
-        <h1>Free Estimates</h1>
-          <form action="mailto:freddyroberts9@gmail.com" method="post" onClick="submit" encType="text/plain">
-            <input type="text" placeholder="Name"/>
-            <input type="text" placeholder="Email or Phone"/>
-            <input type="text" placeholder="City"/>
-            <select type="text">
-              <option value="Residential" selected>Residential</option>
-              <option value="Commercial" selected>Commercial</option>
-              <option disabled selected>Pool Type</option>
-              <input type="submit" placeholder="send" value="submit"/>
+      <main className="estimates">
+          <section className="hero">
+            <h1 className="estimates-title">Free Estimates</h1>
+          <form className="estimate-form" action="mailto:freddyroberts9@gmail.com" method="post" onClick="submit" encType="text/plain">
+            <input className="estimate-info" name="Name" type="text" placeholder="Name"/>
+            <input className="estimate-info" name="Email or Phone" type="text" placeholder="Email or Phone"/>
+            <input className="estimate-info" name="City" type="text" placeholder="City"/>
+            <select className="estimate-info" name="Pool Type" type="text">
+              <option className="pool-types" value="Residential" selected>Residential</option>
+              <option className="pool-types" value="Commercial" selected>Commercial</option>
+              <option className="pool-types" disabled selected>Pool Type</option>
             </select>
+            <button className="estimate-btn" type="submit" value="submit">Send</button>
           </form>
+          </section>
       </main>
     )
   }
--- src/js/homepage.js
+++ src/js/homepage.js
@@ -4,9 +4,11 @@ import React, { PropTypes } from 'react';
 class Home extends React.Component {
   render () {
       return (
-        <section className="hero">
-          <h1 className="title">Swim Clean.</h1>
-        </section>
+        <main className="hero">
+          <section className="hero">
+            <h1 className="title">Swim Clean.</h1>
+          </section>
+        </main>
     )
   }
 }
